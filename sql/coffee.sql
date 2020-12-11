71279cceae0c28ec962ada1b8c1df3a2485512ae
leekyueun <67741402+leekyueun@users.noreply.github.com>
2020-12-11 09:50:34 +0900

    add: insert 작성

--- sql/coffee.sql
+++ sql/coffee.sql
@@ -19,3 +19,38 @@ CREATE TABLE TBL_SALELIST_01 (
     AMOUNT NUMBER(10),
     CONSTRAINT PK_SALELIST PRIMARY KEY (SALENO)
 );
+
+insert into tbl_product_01 values ( 'AA01', '아메리카노', 3000)
+insert into tbl_product_01 values ( 'AA02', '에스프레소', 3500)
+insert into tbl_product_01 values ( 'AA03', '카페라떼', 4000)
+insert into tbl_product_01 values ( 'AA04', '카라멜마끼', 4500)
+insert into tbl_product_01 values ( 'AA05', '카푸치노', 5000)
+insert into tbl_product_01 values ( 'AA06', '초코롤케익', 6000)
+insert into tbl_product_01 values ( 'AA07', '녹차롤케익', 6500)
+insert into tbl_product_01 values ( 'AA08', '망고쥬스', 7000)
+insert into tbl_product_01 values ( 'AA09', '핫초코', 2500)
+
+
+insert into tbl_shop_01 values ( 'S001', '강남점')
+insert into tbl_shop_01 values ( 'S002', '강서점')
+insert into tbl_shop_01 values ( 'S003', '강동점')
+insert into tbl_shop_01 values ( 'S004', '강북점')
+insert into tbl_shop_01 values ( 'S005', '동대문점')
+insert into tbl_shop_01 values ( 'S006', '인천점')
+
+
+insert into tbl_salelist_01 values ( '100001', 'AA01', '20180902', 'S001', 50)
+insert into tbl_salelist_01 values ( '100002', 'AA03', '20180902', 'S002', 40)
+insert into tbl_salelist_01 values ( '100003', 'AA04', '20180902', 'S002', 20)
+insert into tbl_salelist_01 values ( '100004', 'AA04', '20180902', 'S001', 30)
+insert into tbl_salelist_01 values ( '100005', 'AA05', '20180902', 'S004', 40)
+insert into tbl_salelist_01 values ( '100006', 'AA03', '20180902', 'S004', 30)
+insert into tbl_salelist_01 values ( '100007', 'AA01', '20180902', 'S003', 40)
+insert into tbl_salelist_01 values ( '100008', 'AA04', '20180902', 'S004', 10)
+insert into tbl_salelist_01 values ( '100009', 'AA01', '20180902', 'S003', 20)
+insert into tbl_salelist_01 values ( '100010', 'A005', '20180902', 'S003', 30)
+insert into tbl_salelist_01 values ( '100011', 'AA01', '20180902', 'S001', 40)
+insert into tbl_salelist_01 values ( '100012', 'AA03', '20180902', 'S002', 50)
+insert into tbl_salelist_01 values ( '100013', 'AA04', '20180902', 'S002', 50)
+insert into tbl_salelist_01 values ( '100014', 'AA05', '20180902', 'S004', 20)
+insert into tbl_salelist_01 values ( '100015', 'AA01', '20180902', 'S003', 30)
